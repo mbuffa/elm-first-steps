388d0d2b3f72261ff0786e354a99dd8b84ae39b5
Maxime Buffa <mbuffa@gmail.com>
2020-02-25 13:53:28 +0100

    Add the Clock example

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 dice:
 	elm make src/Dice.elm
+
+clock:
+	elm make src/CLock.elm
---
+++ src/Clock.elm
@@ -0,0 +1,102 @@
+-- Show the current time in your time zone.
+--
+-- Read how it works:
+--   https://guide.elm-lang.org/effects/time.html
+--
+-- For an analog clock, check out this SVG example:
+--   https://elm-lang.org/examples/clock
+--
+
+import Browser
+import Html exposing (..)
+import Html.Events exposing (..)
+import Task
+import Time
+
+
+
+-- MAIN
+
+
+main =
+  Browser.element
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { zone : Time.Zone
+  , time : Time.Posix
+  , ticking : Bool
+  }
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( Model Time.utc (Time.millisToPosix 0) True
+  , Task.perform AdjustTimeZone Time.here
+  )
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Tick Time.Posix
+  | AdjustTimeZone Time.Zone
+  | PauseResume
+
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Tick newTime ->
+      if model.ticking then
+        ( { model | time = newTime }
+        , Cmd.none
+        )
+      else
+        ( model, Cmd.none )
+
+    AdjustTimeZone newZone ->
+      ( { model | zone = newZone }
+      , Cmd.none
+      )
+      
+    PauseResume ->
+      ( { model | ticking = not model.ticking }
+      , Cmd.none
+      )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Time.every 1000 Tick  
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  let
+    hour   = String.fromInt (Time.toHour   model.zone model.time)
+    minute = String.fromInt (Time.toMinute model.zone model.time)
+    second = String.fromInt (Time.toSecond model.zone model.time)
+  in
+  div []
+  [ h1 [] [ text (hour ++ ":" ++ minute ++ ":" ++ second) ]
+  , button [ onClick PauseResume ] [ text "Pause/Resume" ]
+  ]
